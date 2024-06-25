2084ec021e3a64402ec24eafe3560cb23144cc9a
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-06-25 10:22:11 +0900

    １３．４

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -5,36 +5,53 @@ public class GameFrame extends MyFrame
 	public void run() 
 	{
 		GameWorld.player=new Player(100,300,0,0);
-		
 		addKeyListener(GameWorld.player);
-		
-		GameWorld.playerBullets = new Vector<PlayerBullet>();
-		
-		GameWorld.enemies = new Vector<Enemy>();
-		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
-		
-		
-		
-		while(true) 
-		{
-			clear();
-			GameWorld.player.draw(this);
-			GameWorld.player.move();
-			movePlayerBullets() ;
+		while(true) {
+			GameWorld.player.x=100;
+			GameWorld.player.y=300;
 			
-			moveEnemies();
-			
-			checkPlayerAndEnemies();
-			
-			checkPlayerBulletsAndEnemies() ;
-			
-			if(GameWorld.enemies.size()==0) 
+			GameWorld.playerBullets = new Vector<PlayerBullet>();
+		
+			GameWorld.enemies = new Vector<Enemy>();
+			GameWorld.enemies.add(new EnemyBase(100,50,1,0));
+			GameWorld.enterPressed=false;
+		
+		
+		
+			while(true) 
 			{
-				setColor(0,0,0);
-				drawString("クリア！",100,200,40);
-			}
+				clear();
+				GameWorld.player.draw(this);
+				GameWorld.player.move();
+				movePlayerBullets() ;
+			
+				moveEnemies();
+			
+				checkPlayerAndEnemies();
+			
+				checkPlayerBulletsAndEnemies() ;
+			
+				if(GameWorld.enemies.size()==0) 
+				{
+					setColor(0,0,0);
+					drawString("クリア！",100,200,40);
+					if(GameWorld.enterPressed) 
+					{
+						break;
+					}
+				}
+				else if(GameWorld.player.y<0) 
+				{
+					setColor(0,0,0);
+					drawString("ゲームオーバー！",50,200,40);
+					if(GameWorld.enterPressed) 
+					{
+						break;
+					}
+				}
  			
-			sleep(0.03);
+				sleep(0.03);
+			}
 		}
 		
 	}
@@ -89,14 +106,14 @@ public class GameFrame extends MyFrame
 			if(checkHit(GameWorld.player,e)) //
 			{
 				System.out.println("やられた！！");
-				GameWorld.player.y = 1000;
+				GameWorld.player.y = -1000;
 			}
 			
 		}
 	}
 	
 	public  void checkPlayerBulletsAndEnemies() 
-	{
+	{	
 		int i=0;
 		
 		while(i<GameWorld.playerBullets.size()) 
