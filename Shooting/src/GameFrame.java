beacff1d9f76c1dd232443f5bd9d206322b61898
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-06-25 10:29:16 +0900

    13.6

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -8,13 +8,14 @@ public class GameFrame extends MyFrame
 		addKeyListener(GameWorld.player);
 		GameWorld.stage=1;
 		while(true) {
+			
 			GameWorld.player.x=100;
 			GameWorld.player.y=300;
 			
 			GameWorld.playerBullets = new Vector<PlayerBullet>();
 		
 			GameWorld.enemies = new Vector<Enemy>();
-			GameWorld.enemies.add(new EnemyBase(100,50,1,0));
+			GameWorld.enemies.add(new EnemyBase(100,50,GameWorld.stage,0));
 			GameWorld.enterPressed=false;
 		
 		
